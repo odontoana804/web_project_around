29f1b051a6f5b98eb45fbe6fc0a081a7def2a61f
Ana Maria Vargas <odontoana804@gmail.com>
2024-05-13 08:57:14 -0500

    refactorizar codigo - funcion de eliminar trajeta del arreglo

--- scripts/script.js
+++ scripts/script.js
@@ -48,102 +48,52 @@ const initialCards = [
   },
 ];
 
-//carga las tarjetas creadas desde el arreglo
+//Función para cargar las tarjetas creadas desde el arreglo inicial
 function loadCards() {
   let createdCards = "";
   for (let initialCard of initialCards) {
-    createdCards += addPlace(initialCard.name, initialCard.url);
+    initialCard.id = Math.floor(Math.random() * new Date().getTime() * 3).toString(); //crea un Id único para las tarjetas creadas desde el arreglo inicial
+    createdCards += addPlace(initialCard.name, initialCard.url, initialCard.id);
   }
   cardsContainer.content = createdCards;
 }
 
-//muestra las ventanas popUps
-function showPopUp(popup, overlay) {
-  popup.classList.add("popup_opened");
-  overlay.classList.add("overlay_opened");
+//Función para mostrar u ocultar el popUp de agregar nuevas tarjetas y edición del perfil
+function ShowAndClosePopUp(popup, overlay) {
+  popup.classList.toggle("popup_opened");
+  overlay.classList.toggle("overlay_opened");
 }
 
-//cierra las ventanas popUps
-function closePopUp(popup, overlay) {
-  popup.classList.remove("popup_opened");
-  overlay.classList.remove("overlay_opened");
+//Función para mostrar u ocultar el popUp de la visualización de la imagen
+function ShowAndClosePopUpImage(popup, overlay) {
+  popup.classList.toggle("img-popup_opened");
+  overlay.classList.toggle("overlay_opened");
 }
 
-//edita los campos del nombre del perfil y la profesión
+//Función para editar los campos del nombre del perfil y la profesión
 function editProfile(name, about) {
   profileName.textContent = name;
   profileDescription.textContent = about;
 }
 
-//muesta la ventana de visaulización de las imagenes
-function showPopUpImage(popup, overlay) {
-  popup.classList.add("img-popup_opened");
-  overlay.classList.add("overlay_opened");
-}
-
-//cierre la ventana de visualización de las imagenes
-function closePopUpImage(popup, overlay) {
-  popup.classList.remove("img-popup_opened");
-  overlay.classList.remove("overlay_opened");
-}
-
-//revisar esta función
-function deleteCard() {
-  const trashButtons = document.querySelectorAll(".elements__card-btn-trash");
-  for (const button of trashButtons) {
-    button.addEventListener("click", (id) => {
-      let indiceEliminar = initialCards.findIndex(
-        (initialCard) => initialCard.id === id
-      );
-      initialCards.splice(indiceEliminar, 1);
-      const cardItem = button.closest(".elements__card");
-      cardItem.remove();
-    });
-  }
-}
-
 //función para agregar tarjetas de lugares
-function addPlace(name, url) {
+function addPlace(name, url, id) {
   //creación de tarjetas clonando contenido de template
   const cardTemplate = document.querySelector("#card-template").content;
   const newCard = cardTemplate.querySelector(".elements__card").cloneNode(true);
   newCard.querySelector(".elements__card-photo-imagen").src = url;
   newCard.querySelector(".elements__card-photo-imagen").alt = name;
+  newCard.querySelector(".elements__card-photo-imagen").setAttribute("id", id )
   newCard.querySelector(".elements__card-title").textContent = name;
   cardsContainer.prepend(newCard);
-
-  //llamada a la función para eliminar una tarjeta
-  deleteCard();
-
-  //codigo para botón de like con event.target
-  const likeButton = newCard.querySelector(".elements__card-btn-hearth");
-  likeButton.addEventListener("click", (evt) => {
-    evt.target.classList.toggle("elements__card-btn-hearth_active");
-  });
-
-  //código para agregar la función de la previsualización de imagen
-  const imagePreview = newCard.querySelector(".elements__card-photo-imagen");
-  imagePreview.addEventListener("click", () => {
-    showPopUpImage(popupImage, popupOverlay);
-    const imageValue = popupImage.querySelector(".img-popup__preview");
-    imageValue.src = url;
-    const imageTitle = popupImage.querySelector(".img-popup__title");
-    imageTitle.textContent = name;
-  });
-
-  //código para cerrar el popup de previsualización de imagen con el cancel buton
-  const cancelButtonPopupImage = popupImage.querySelector("#btn-close-image");
-  cancelButtonPopupImage.addEventListener("click", () =>
-    closePopUpImage(popupImage, popupOverlay)
-  );
 }
 
-//llamada a la función para cargar las tarjetas ya creadas
+//llamada a la función para cargar las tarjetas ya creadas en el arrgelo inicial
 loadCards();
 
 //abre la ventana popup Profile al dar click en el icono del lápiz (editar)
 editButtonProfile.addEventListener("click", () => {
-  showPopUp(popupProfile, popupOverlay);
+  ShowAndClosePopUp(popupProfile, popupOverlay);
   popupNameProfile.value = document.querySelector(".profile__name").textContent;
   popupAboutProfile.value = document.querySelector(
     ".profile__description"
@@ -152,33 +102,64 @@ editButtonProfile.addEventListener("click", () => {
 
 //cierra la ventana popUp Profile al dar click en el icono de X (cerrar)
 cancelButtonProfile.addEventListener("click", () =>
-  closePopUp(popupProfile, popupOverlay)
+  ShowAndClosePopUp(popupProfile, popupOverlay)
 );
 
 //cambia el contenido de los campos de nombre y acercaDe del Profile al dar click en el botón Guardar
 confirmButtonProfile.addEventListener("click", () => {
   if (popupNameProfile.value !== "" && popupAboutProfile.value !== "") {
     editProfile(popupNameProfile.value, popupAboutProfile.value);
-    closePopUp(popupProfile, popupOverlay);
+    ShowAndClosePopUp(popupProfile, popupOverlay);
   }
 });
 
 //abre la ventana popup Place al dar click en el icono del + (agregar)
 addButtonPlace.addEventListener("click", () => {
-  showPopUp(popupPlace, popupOverlay);
+  ShowAndClosePopUp(popupPlace, popupOverlay);
   popupNamePlace.value = "";
   popupUrlPlace.value = "";
 });
 
 //cierra la ventana popUp Place al dar click en el icono de X (cerrar)
 cancelButtonPlace.addEventListener("click", () =>
-  closePopUp(popupPlace, popupOverlay)
+  ShowAndClosePopUp(popupPlace, popupOverlay)
 );
 
 //agrega una nueva tarjeta a lugares al daer click en el botón Crear
 confirmButtonPlace.addEventListener("click", () => {
   if (popupUrlPlace.value !== "" && popupNamePlace.value !== "") {
-    addPlace(popupNamePlace.value, popupUrlPlace.value);
-    closePopUp(popupPlace, popupOverlay);
+    const idNewPLace = Math.floor(Math.random()* new Date().getTime() * 3).toString() //crea un Id único para las nuevas tarjetas
+    addPlace(popupNamePlace.value, popupUrlPlace.value, idNewPLace);
+    initialCards.push({ name: popupNamePlace.value, url: popupUrlPlace.value, id : idNewPLace });
+    ShowAndClosePopUp(popupPlace, popupOverlay);
+  }
+});
+
+//código para botón de like con event.target y agregar la función de la previsualización de imagen y eliminar un elemento del DOM y del arreglo initialCards
+cardsContainer.addEventListener( "click", (evt) => {
+  if (evt.target.classList.contains("elements__card-btn-hearth")) {
+    evt.target.classList.toggle("elements__card-btn-hearth_active");
+  }
+  if (evt.target.classList.contains("elements__card-photo-imagen")){
+    ShowAndClosePopUpImage(popupImage, popupOverlay);
+    const imageValue = popupImage.querySelector(".img-popup__preview");
+    imageValue.src = evt.target.currentSrc;
+    imageValue.alt = evt.target.alt;
+    const imageTitle = popupImage.querySelector(".img-popup__title");
+    imageTitle.textContent = evt.target.alt;
+  }
+  if (evt.target.classList.contains("elements__card-btn-trash")){
+    evt.target.closest(".elements__card").remove();
+    let indiceEliminar = initialCards.findIndex(
+      (initialCard) => initialCard.id === evt.target.previousElementSibling.id
+    );
+    initialCards.splice(indiceEliminar, 1);
+  }
+});
+
+//código para cerrar el popup de previsualización de imagen con el cancel button
+popupImage.addEventListener( "click", (evt) => {
+  if (evt.target.classList.contains("img-popup__btn-close")) {
+    ShowAndClosePopUpImage(popupImage, popupOverlay)
   }
 });
