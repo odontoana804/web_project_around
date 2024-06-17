de8d3e6af349791820ead5360c55a21a4cbfbae3
Ana Maria Vargas <odontoana804@gmail.com>
2024-06-17 11:44:18 -0500

    Transforma la clase Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,11 @@
-import PopupWithImage from "./PopupWithImage.js";
-
-const popupImageConfig = {
-  openPopupClass: "img-popup_opened",
-  openOverlayClass: "overlay_opened",
-  closeButtonSelector: ".img-popup__btn-close",
-  popupUbication: ".img-popup",
-  imagePreviewClass: ".img-popup__preview",
-  imageTitleClass: ".img-popup__title",
-  popupOverlay: document.querySelector(".overlay"),
-};
-
 export class Card {
-  constructor( data, cardSelector, array) {
+  constructor( data, cardSelector, array, handleCardClick) {
     this._id = this._createUniqueId()
     this._name = data.name;
     this._url = data.url;
     this._cardSelector = cardSelector;
     this._array = array;
+    this._handleCardClick = handleCardClick;
   };
 
   _createUniqueId () {
@@ -40,20 +29,7 @@ export class Card {
       };
       //evento para mostrar el popUp de la visualización de la imagen
       if (evt.target.classList.contains("elements__card-photo-imagen")){
-        const popupImage = new PopupWithImage("#popupImageTemplate", "#popup__image", popupImageConfig, {
-          name: evt.target.alt,
-          image: evt.target.currentSrc
-        })
-        const popup = popupImage.generatePopup()
-        popupImage.open()
-        document.querySelector(popupImageConfig.popupUbication).prepend(popup)
-       /*  this._showPopUpImage(this._popupImage, this._popupOverlay);
-        const imageValue = this._popupImage.querySelector(".img-popup__preview");
-        imageValue.src = evt.target.currentSrc;
-        imageValue.alt = evt.target.alt;
-        const imageTitle = this._popupImage.querySelector(".img-popup__title");
-        imageTitle.textContent = evt.target.alt;
-        document.addEventListener("keydown", this._closePopUpImageEscapeKey); */
+        this._handleCardClick(evt);
       };
       //evento para funcionalidad del boton de eliminar
       if (evt.target.classList.contains("elements__card-btn-trash")){
@@ -62,23 +38,10 @@ export class Card {
           (element) => element.id === evt.target.previousElementSibling.id
         );
         this._array.splice(indiceEliminar, 1);
+        console.log(this._array)
       };
     });
   };
-  //Función para mostrar el popUp de la visualización de la imagen
-/*   _showPopUpImage(popup, overlay) {
-    popup.classList.add("img-popup_opened");
-    overlay.classList.add("overlay_opened");
-  }; */
-
- /*  _closePopUpImageEscapeKey (evt) {
-    this._popupImage = document.querySelector("#popup__image");
-    this._popupOverlay = document.querySelector(".overlay");
-    if (evt.key === "Escape") {
-      this._popupImage.classList.remove("img-popup_opened");
-      this._popupOverlay.classList.remove("overlay_opened");
-    };
-  }; */
 
   //función para crear la tarjeta
   generateCard() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,56 @@
 import { Card } from "../components/Card.js";
-import { FormValidator } from "../components/FormValidator.js";
 import PopupWithForms from "../components/PopupWithForms.js";
+import PopupWithImage from "../components/PopupWithImage.js";
 import Section from "../components/Section.js";
 import { UserInfo } from "../components/UserInfo.js";
-import { initialCards, cardsListSection, profileName, profileDescription } from "../scripts/utils.js";
+import {
+  initialCards,
+  cardsListSection,
+  profileName,
+  profileDescription,
+} from "../scripts/utils.js";
+
+const popupImageConfig = {
+  openPopupClass: "img-popup_opened",
+  openOverlayClass: "overlay_opened",
+  closeButtonSelector: ".img-popup__btn-close",
+  popupUbication: ".img-popup",
+  imagePreviewClass: ".img-popup__preview",
+  imageTitleClass: ".img-popup__title",
+  popupOverlay: document.querySelector(".overlay"),
+};
 
 //Función para cargar las tarjetas creadas desde el arreglo inicial
-const cardsList = new Section({
-  data: initialCards,
-  renderer: (initialCard) => {
-    const card = new Card(initialCard, "#card-template", initialCards);
-    initialCard.id = card._id;
+const cardsList = new Section(
+  {
+    data: initialCards,
+    renderer: (initialCard) => {
+      const card = new Card(
+        initialCard,
+        "#card-template",
+        initialCards,
+        (evt) => {
+          const popupImage = new PopupWithImage(
+            "#popupImageTemplate",
+            "#popup__image",
+            popupImageConfig,
+            {
+              name: evt.target.alt,
+              image: evt.target.currentSrc,
+            }
+          );
+          const popup = popupImage.generatePopup();
+          popupImage.open();
+          document
+            .querySelector(popupImageConfig.popupUbication)
+            .prepend(popup);
+        }
+      );
+      initialCard.id = card._id;
 
-    const cardElement = card.generateCard();
+      const cardElement = card.generateCard();
 
-    cardsList.setItem(cardElement);
+      cardsList.setItem(cardElement);
     },
   },
   cardsListSection
@@ -23,16 +59,15 @@ const cardsList = new Section({
 //renderizar las 6 tarjetas iniciales
 cardsList.renderItems();
 
-
 const popupWithFormConfig = {
-  openPopupClass : "popup_opened",
+  openPopupClass: "popup_opened",
   openOverlayClass: "overlay_opened",
   closeButtonSelector: ".popup__btn-close",
   popupUbication: ".popup",
   popupOverlay: document.querySelector(".overlay"),
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__btn-submit",
-}
+};
 
 export const userInfo = new UserInfo(profileName, profileDescription);
 
@@ -41,8 +76,8 @@ export const popupProfile = new PopupWithForms(
   "#popup__profile",
   popupWithFormConfig,
   (formData) => {
-    userInfo.setUserInfo(formData)
-    popupProfile.close()
+    userInfo.setUserInfo(formData);
+    popupProfile.close();
   }
 );
 
@@ -51,9 +86,25 @@ export const popupPlace = new PopupWithForms(
   "#popup__place",
   popupWithFormConfig,
   (formData) => {
-    const card = new Card(formData, "#card-template");
+    const card = new Card(formData, "#card-template", initialCards, (evt) => {
+      const popupImage = new PopupWithImage(
+        "#popupImageTemplate",
+        "#popup__image",
+        popupImageConfig,
+        {
+          name: evt.target.alt,
+          image: evt.target.currentSrc,
+        }
+      );
+
+      const popup = popupImage.generatePopup();
+      popupImage.open();
+      document.querySelector(popupImageConfig.popupUbication).prepend(popup);
+    });
+    formData.id = card._id;
+    initialCards.push(formData);
     const cardElement = card.generateCard();
     cardsList.setItem(cardElement);
-    popupPlace.close()
+    popupPlace.close();
   }
 );
